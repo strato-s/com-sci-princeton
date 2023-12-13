dac2537a0110c5535aa3039a1beece45e5c42885
strato-s <nigam2614@gmail.com>
2023-12-13 23:22:21 +0530

    assignment-1

---
+++ GreatCircle.java
@@ -0,0 +1,18 @@
+public class GreatCircle {
+    public static void main(String[] args) {
+        double a = Math.toRadians(Double.parseDouble(args[0]));
+        double b = Math.toRadians(Double.parseDouble(args[1]));
+        double c = Math.toRadians(Double.parseDouble(args[2]));
+        double d = Math.toRadians(Double.parseDouble(args[3]));
+
+        double distance;
+
+        double t1 = Math.pow(Math.sin((c - a) / 2), 2);
+        double t2 = Math.cos(a) * Math.cos(c)
+                * Math.pow(Math.sin((d - b) / 2), 2);
+
+        distance = (2 * 6371.0) * Math.asin(Math.sqrt(t1 + t2));
+
+        System.out.println(distance + " kilometres");
+    }
+}
